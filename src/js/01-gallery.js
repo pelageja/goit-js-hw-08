fc26d83a576211510bd67dcbac2149b6c9b4118d
pelageja <29102014n@gmail.com>
2022-07-12 17:01:52 +0300

    hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,36 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const ulRef = document.querySelector('.gallery');
+
+const cartMarkup = createPictureCartMarkup(galleryItems);
+ulRef.insertAdjacentHTML('beforeend', cartMarkup);
+
+//Создаем функцию для создания разметки
+
+function createPictureCartMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+   <a class="gallery__link" href="${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+     />
+   </a>
+ </div>`;
+    })
+    .join('');
+};
+
+new SimpleLightbox(".gallery a", {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+   import throttle from 'lodash.throttle';
+
+
+
+    const iframe = document.querySelector('iframe');
+     const player = new Player(iframe);
+
+   
+    player.on('timeupdate', throttle(onPlay, 1000));  
+
+player.on('timeupdate', function(data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+    player.setCurrentTime(JSON.parse(savedTime))
+}
+
+
+
+
+
+
+
 
